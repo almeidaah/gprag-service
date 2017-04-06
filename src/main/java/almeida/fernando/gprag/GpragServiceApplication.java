7e7b3715758f66baeab4ffdc2131c4e30e517109
almeidaah <almeidascvn@gmail.com>
2017-04-05 22:19:43 -0300

    commit to heroku service(override error page)

--- src/main/java/almeida/fernando/gprag/GpragServiceApplication.java
+++ src/main/java/almeida/fernando/gprag/GpragServiceApplication.java
@@ -2,11 +2,28 @@ package almeida.fernando.gprag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
-public class GpragServiceApplication {
+public class GpragServiceApplication implements ErrorController {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GpragServiceApplication.class, args);
+	}
+	
+	@RequestMapping(value="/error")
+	@ResponseBody
+	public String error(){
+		return "G-PRAG Service APP (Heroku)\n Author : Fernando Almeida";
+	}
+	
+	@Override
+	public String getErrorPath() {
+		return "/error";
 	}
 }
