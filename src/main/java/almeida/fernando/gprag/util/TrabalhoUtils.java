4a7b48df1026e1cdb422aa99f91b2a242e047023
almeidaah <almeidascvn@gmail.com>
2017-04-12 22:26:13 -0300

    alter gitignore

--- src/main/java/almeida/fernando/gprag/util/TrabalhoUtils.java
+++ src/main/java/almeida/fernando/gprag/util/TrabalhoUtils.java
@@ -6,14 +6,14 @@ import almeida.fernando.gprag.model.Trabalho;
 
 public class TrabalhoUtils {
 	
-	private static final String ID_CLIENTE = "id";
+	private static final String ID_CLIENTE = "idCliente";
 	private static final String TIPO_TRABALHO = "tipoTrabalho";
 	private static final String VALOR_TOTAL = "valorTotal";
 	private static final String REPRESENTANTE = "representante";
 	private static final String APLICADOR = "aplicador";
 	
 	public static Trabalho popularTrabalho(Map<String, String> parameters){
-		
+
 		Trabalho t = new Trabalho();
 		t.setIdCliente(parameters.get(TrabalhoUtils.ID_CLIENTE));
 		t.setValor(Double.parseDouble(parameters.get(TrabalhoUtils.VALOR_TOTAL)));
@@ -21,8 +21,10 @@ public class TrabalhoUtils {
 		t.setAplicador(parameters.get(TrabalhoUtils.APLICADOR));
 		
 		//TODO
-		//t.setTipoTrabalho(parameters.get(TrabalhoUtils.TIPO_TRABALHO));
+//		String tipoTrabalhoRealizado = (String)parameters.get(TrabalhoUtils.TIPO_TRABALHO);
 		//t.setTipoTrabalho((Map<String, Boolean>)parameters.g(TrabalhoUtils.TIPO_TRABALHO));
+		
+		//{"text":"DTT","enabled":true}
 		
 		return t;
 	}
