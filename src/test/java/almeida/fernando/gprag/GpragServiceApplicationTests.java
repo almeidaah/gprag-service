edc243f77f814ce557641aff158cddaff35e3f16
Fernando Almeida <almeidascvn@gmail.com>
2017-05-11 14:38:56 -0300

    Adding JUnit simple tests

--- src/test/java/almeida/fernando/gprag/GpragServiceApplicationTests.java
+++ src/test/java/almeida/fernando/gprag/GpragServiceApplicationTests.java
@@ -1,16 +1,54 @@
 package almeida.fernando.gprag;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import almeida.fernando.gprag.model.Cliente;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class GpragServiceApplicationTests {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	/*
+	 * o Endpoint / está sem serviços, não deve retornar nada.
+	 */
+
+	@SuppressWarnings("unchecked")
 	@Test
-	public void contextLoads() {
+	public void homeTestStatus() {
+		Map<String, String> homeReturn = this.restTemplate.getForObject("/", HashMap.class);
+		assertEquals(404, homeReturn.get("status"));
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void homeTestMessage() {
+		Map<String, String> homeReturn = this.restTemplate.getForObject("/", HashMap.class);
+		assertEquals("No message available", homeReturn.get("message"));
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testGetClients() {
+		List<Cliente> clientes = (List<Cliente>) this.restTemplate.getForObject("/gprag/v1/clients", ArrayList.class);
+		assertTrue(clientes.size() > 0);
 	}
 
 }
